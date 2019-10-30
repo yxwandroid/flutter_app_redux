bc3dd59fc7f778d7d45d66e05cc9b1440f7e2995
wilson <yxw_android@163.com>
2019-10-30 18:09:52 +0800

    update

--- lib/av_redux_list_screen.dart
+++ lib/av_redux_list_screen.dart
@@ -3,14 +3,11 @@ import 'package:flutter_app_redux/redux/avlist_state.dart';
 import 'package:flutter_app_redux/redux/main_state.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
-class AVReduxListScreen extends StatelessWidget {
 
+class AVReduxListScreen extends StatelessWidget {
   final Store<MainState> store;
 
-  AVReduxListScreen({
-    Key key,
-    this.store
-  }): super(key:key);
+  AVReduxListScreen({Key key, this.store}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,36 +18,32 @@ class AVReduxListScreen extends StatelessWidget {
         ),
         body: new Column(
           children: <Widget>[
-//                new StoreConnector<MainState, List<AVList>>(
-////                  converter: (store) => store.state.data,
-//                  builder: (BuildContext context, data){
-//                    return Text("ee");
-//                  },
-//                ),
-           new StoreConnector<MainState,AVListState>(converter: (store){
-             return   store.state.avListState;
-           },builder: (BuildContext context,data){
-             List<AVList> avList = data.data;
+            new StoreConnector<MainState, AVListState>(
+              converter: (store) {
+                return store.state.avListState;
+              },
+              builder: (BuildContext context, data) {
+                List<AVList> avList = data.data;
 
-             return new Container(
-               height: 500.0,
-               child: ListView.builder(
-                 itemCount: avList.length,
-                 itemBuilder: (BuildContext context, int position){
-                   return new Padding(
-                     padding: EdgeInsets.all(10.0),
-                     child: new Row(
-                       children: <Widget>[
-                         new Text(avList[position].name),
-                         new Icon(avList[position].icon, color: Colors.blue)
-                       ],
-                     ),
-                   );
-                 },
-               ),
-             );
-
-           },),
+                return new Container(
+                  height: 500.0,
+                  child: ListView.builder(
+                    itemCount: avList.length,
+                    itemBuilder: (BuildContext context, int position) {
+                      return new Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: new Row(
+                          children: <Widget>[
+                            new Text(avList[position].name),
+                            new Icon(avList[position].icon, color: Colors.blue)
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
             new Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
@@ -58,26 +51,21 @@ class AVReduxListScreen extends StatelessWidget {
                   color: Colors.blue,
                   child: new Text(
                     '更新',
-                    style: new TextStyle(
-                        color: Colors.white
-                    ),
+                    style: new TextStyle(color: Colors.white),
                   ),
-                  onPressed: (){
-                    store.dispatch(AddAVListAction(AVList("wilson",Icons.android)));
+                  onPressed: () {
+                    store.dispatch(
+                        AddAVListAction(AVList("wilson", Icons.android)));
                   },
                 ),
                 new RaisedButton(
                   color: Colors.blue,
                   child: new Text(
                     '删除最后一项',
-                    style: new TextStyle(
-                        color: Colors.white
-                    ),
+                    style: new TextStyle(color: Colors.white),
                   ),
-                  onPressed: (){
-                    store.dispatch(
-                        new RemoveAVListAction()
-                    );
+                  onPressed: () {
+                    store.dispatch(new RemoveAVListAction());
                   },
                 )
               ],
