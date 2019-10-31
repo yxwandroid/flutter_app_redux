ac436a406f981bb54a8a45a48aa16233b9b56c45
wilson <yxw_android@163.com>
2019-10-31 10:21:07 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_redux/av_redux_list_screen.dart';
 import 'package:flutter_app_redux/count_state.dart';
-import 'package:flutter_app_redux/redux/index.dart';
-import 'package:flutter_app_redux/redux/main_state.dart';
-import 'package:flutter_app_redux/store/store_manager.dart';
 import 'package:flutter_app_redux/top_screen.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
-//
-//void main() {
-//  final store = Store<CountState>(reducer, initialState: CountState.initState());
-//  runApp(new MyApp(store));
-//}
-//
-//class MyApp extends StatelessWidget {
-//  final Store<CountState> store;
-//
-//  MyApp(this.store);
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return StoreProvider<CountState>(
-//      store: store,
-//      child: new MaterialApp(
-//        title: 'Flutter Demo',
-//        theme: new ThemeData(
-//          primarySwatch: Colors.blue,
-//        ),
-//        home: TopScreen(),
-//      ),
-//    );
-//  }
-//}
 
 void main() {
-  runApp(new MyApp());
+
+
+  final store = Store<CountState>(reducer, initialState: CountState.initState());
+  runApp(new MyApp(store));
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  final Store<CountState> store;
 
-class _MyAppState extends State<MyApp> {
-  final store = new Store<MainState>(appReducer,
-      initialState: MainState(
-          userState: UserState.initStore(),
-          avListState: AVListState.initStore()));
-
-  @override
-  void initState() {
-    super.initState();
-    print("初始化应用的时候初始化store管理者");
-    StoreManager.setStore(store);
-  }
+  MyApp(this.store);
 
   @override
   Widget build(BuildContext context) {
-    return StoreProvider<MainState>(
+    return StoreProvider<CountState>(
       store: store,
       child: new MaterialApp(
         title: 'Flutter Demo',
         theme: new ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: AVReduxListScreen(),
+        home: TopScreen(),
       ),
     );
   }
--- lib/redux/index.dart
+++
@@ -1,3 +0,0 @@
-export 'avlist_state.dart';
-export 'user_state.dart';
-export 'main_state.dart';
